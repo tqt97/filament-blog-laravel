721fdc5194f4eb992d4907ee2b96be23e0b96269
QuocTuanIT <kutuanonline199@gmail.com>
2023-09-14 21:46:21 +0700

    create model and migration

---
+++ database/migrations/2023_09_14_142857_create_article_category_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Models\Article;
+use App\Models\Category;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('article_category', function (Blueprint $table) {
+            $table->foreignIdFor(Article::class);
+            $table->foreignIdFor(Category::class);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('article_category');
+    }
+};
